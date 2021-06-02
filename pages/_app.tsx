32eef1ccd1bb80e77a0d8d61f1c0af6936313780
Victor Dias <diasdev23@gmail.com>
2021-06-02 02:34:50 -0300

    chore: config styled components and _document file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
-import { AppProps } from "next/app";
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { GlobalStyles } from 'styles/globals'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Victor Dias | Front-end Developer</title>
+        <meta
+          name="description"
+          content="Este é um simples portfólio para uma breve apresentação sobre mim, Victor Dias, como desenvolvedor."
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp;
+export default App
